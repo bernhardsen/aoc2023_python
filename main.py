253a33f9c880940b66985a09e1a77f75939f48c7
bernhardsen <bernhardsen@gmail.com>
2023-12-03 10:49:09 +0100

    Day 3 + added README.md

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 from day1 import day1
 from day2 import day2
+from day3 import day3
 
 print('Advent of Code 2023!')
 if len(sys.argv) == 2:
@@ -11,6 +12,8 @@ if len(sys.argv) == 2:
             day1()
         case "2":
             day2()
+        case "3":
+            day3()
         case _:
             print(f"Day {sys.argv[1]} not implemented yet")
 else:
